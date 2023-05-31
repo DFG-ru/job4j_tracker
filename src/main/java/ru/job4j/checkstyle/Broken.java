6edff94fad2466429923144af786e4d19ba5841a
DFG-job <cemka5@mail.ru>
2023-05-31 09:45:28 +0400

    6. Правила Checkstyle [#504981  [#504981 #419675]]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,24 +1,23 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
+    private static final String NEW_VALUE = "";
     private int sizeOfEmpty = 10;
-    public String surname;
-    public static final String NEW_VALUE = "";
-    String name;
+    private String surname;
+    private String name;
 
-    void echo() {
+    public Broken() {
     }
 
-    void media(Object obj) {
+    public void echo() {
+    }
+
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e) {
-
-    }
-
-    Broken() {
+    public void method(int a, int b, int c, int d, int e) {
     }
 }
