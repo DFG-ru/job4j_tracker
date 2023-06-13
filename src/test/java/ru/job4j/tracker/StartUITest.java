45fc832eaa19a70d79bd63680eefc1df55ce6fe0
DFG-job <cemka5@mail.ru>
2023-06-13 11:42:52 +0400

    12.0. Написать тесты на StartUI. [#785  [#785]]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,46 +6,68 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StartUITest {
     @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
+    public void whenCreateItem() {
+        Input in = new StubInput(
+                new String[]{"0", "Item name", "1"}
+        );
         Tracker tracker = new Tracker();
-        UserAction action = new CreateAction();
-        action.execute(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName()).isEqualTo(expected.getName());
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
     }
 
     @Test
     public void whenEditItem() {
+        Input in = new StubInput(
+                new String[]{"0", "1", "Edited item", "1"}
+        );
         Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        UserAction action = new EditAction();
-        tracker.add(item);
-        String[] answers = {
-                String.valueOf(item.getId()),
-                "edited item"
+        UserAction[] actions = {
+                new EditAction(),
+                new ExitAction()
         };
-        action.execute(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertThat(edited.getName()).isEqualTo("edited item");
+        Item item = new Item("Item for edit");
+        tracker.add(item);
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Edited item");
+    }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Input in = new StubInput(
+                new String[]{"0", "Replaced item", "1", "1"}
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        Item item = new Item("Item to replace");
+        tracker.add(item);
+        new StartUI().init(in, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Replaced item");
     }
 
     @Test
     public void whenDeleteItem() {
+        Input in = new StubInput(
+                new String[]{"0", "2", "1"}
+        );
         Tracker tracker = new Tracker();
         Tracker expectedTracker = new Tracker();
-        UserAction action = new DeleteAction();
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
         Item item1 = new Item("item1");
         Item item2 = new Item("item2");
         tracker.add(item1);
         tracker.add(item2);
         expectedTracker.add(item1);
-        String[] answers = {
-                String.valueOf(item2.getId())
-        };
-        action.execute(new StubInput(answers), tracker);
+        new StartUI().init(in, tracker, actions);
         assertThat(tracker.findById(item2.getId())).isNull();
         assertThat(tracker.findAll()).isEqualTo(expectedTracker.findAll());
     }
