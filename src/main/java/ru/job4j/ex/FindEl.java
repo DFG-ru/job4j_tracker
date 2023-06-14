7fd810643c6c556089fc51a0c843a3d8dd396f91
DFG-job <cemka5@mail.ru>
2023-06-14 09:58:33 +0400

    0.4. Пользовательские исключения. [#219367  [#219367]]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Элемент в массиве не найден");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException {
+        String[] strArr = new String[]{"bob", "bib", "beb"};
+        try {
+            System.out.println(indexOf(strArr, "bibe"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
