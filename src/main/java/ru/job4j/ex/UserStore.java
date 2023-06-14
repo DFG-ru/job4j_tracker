f3c6c483f6882d34098a943cb8d9c4e48c24c0d2
DFG-job <cemka5@mail.ru>
2023-06-14 16:30:28 +0400

    0.5. Иерархия исключений и множественный catch. [#219368  [#219368 #423003]]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -12,7 +12,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (user.isValid()) {
+        if (user.isValid() && user.getUsername().length() >= 3) {
             return true;
         }
         throw new UserInvalidException("Пользователь не валидный");
